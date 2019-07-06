4dd09b841456063e96579393e8274f73f11dc3b7
Sandro Almeida <sandro001@gmail.com>
2019-07-06 19:31:49 -0300

    Using enum

--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -22,8 +22,14 @@ export class NegociacaoController {
 
         event.preventDefault();
 
+        let date = new Date(this._inputData.val().replace(/-/g,','));
+        if (!this._ehDiaUtil(date)) {
+            this._mensagemView.update('Negociações são permitidas apenas em dias úteis.')
+            return;
+        }
+
         const negociacao = new Negociacao(
-            new Date(this._inputData.val().replace(/-/g,',')),
+            new Date(date),
             parseInt(this._inputQuantidade.val()),
             parseFloat(this._inputValor.val()));
         
@@ -42,4 +48,18 @@ export class NegociacaoController {
 
         console.log(negociacao);
     }
+
+    private _ehDiaUtil(date : Date) : boolean {
+        return date.getDay() != DiaDaSemana.Sabado && date.getDay() != DiaDaSemana.Domingo;
+    }
+}
+
+enum DiaDaSemana {
+    Domingo, //Domingo = 1, //if you assign a value to a enum member, then the remaining members use this value as a base value and are incremented by 1
+    Segunda,
+    Terca,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado
 }
