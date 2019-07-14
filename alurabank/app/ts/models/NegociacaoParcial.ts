2a1f531c3c41fb5cfd8d324e73e0e4fdf9cb6d75
Sandro Almeida <sandro001@gmail.com>
2019-07-14 15:40:11 -0300

    Using external API interface

---
+++ alurabank/app/ts/models/NegociacaoParcial.ts
@@ -0,0 +1,4 @@
+export interface NegociacaoParcial { //interface that defines the properties of the external API
+    vezes: number; //private, protected and public modifiers cannot be used in interface properties
+    montante: number;
+}
